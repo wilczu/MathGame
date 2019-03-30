445283641a19e1bc4698dddeae1e7ccb153e073c
wilczu <kacpersoja1@gmail.com>
2019-03-30 22:17:32 +0000

    Adding return button from info section to main menu.

--- js/navAnimation.js
+++ js/navAnimation.js
@@ -1,8 +1,8 @@
 function info() {
-    fadeIn();
+    infoFadeIn();
 }
 
-function fadeIn() {
+function infoFadeIn() {
     let mainCont = document.getElementById('main-container');
     let infoCont = document.getElementById('info-container');
     mainCont.style.transition = 'opacity 1s';
@@ -18,5 +18,22 @@ function fadeIn() {
         },50);
 
     }, 1000);
+}
 
+function infoFadeOut() {
+    let mainCont = document.getElementById('main-container');
+    let infoCont = document.getElementById('info-container');
+    infoCont.style.transition = 'opacity 1s';
+    infoCont.style.opacity = '0';
+
+    setTimeout(() => {
+        mainCont.style.display = 'block';
+        infoCont.style.display = "none";
+
+        setTimeout(() => {
+            mainCont.style.transition = 'opacity 1s';
+            mainCont.style.opacity = '1';
+        },50);
+
+    }, 1000);
 }
